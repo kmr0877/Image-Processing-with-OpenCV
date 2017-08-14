d75ad5eedef603e2eeef8d54edf1481ee6615552
Mohan <kmr0877@gmail.com>
2017-08-14 10:05:12 +1000

    Improved contrast and resolution

--- imageprocessing.py
+++ imageprocessing.py
@@ -16,33 +16,71 @@
 # #img[100:150, 100:150] = [255,255,255]
 # cv2.imshow('Figure',img)
 import cv2
-import os
-import sys
 import numpy as np
 import sys
 
 def removeBorder(img):
     return img[30:len(img)-20 , 30 : len(img[0]) - 20]
 
-def reconstruct(img):
-    img2 = np.zeros((len(img) / 3 , len(img[0]) , 3), np.uint8)
+def getColor(blue,green,red,level):
+    img2 = np.zeros((len(red) , len(red[0]) , 3), np.uint8)
     for i in range(len(img2)):
         for j in range(len(img2[0])):
-            img2[i][j][0] = img[i + (2 * len(img2))][j]
-            img2[i][j][1] = img[i + len(img2) + 4][j]
-            img2[i][j][2] = img[i + 4][j]
+            img2[i][j][0] = blue[i][j]/(level + 1)
+            img2[i][j][1] = green[i][j]/(level + 1)
+            img2[i][j][2] = red[i][j]/(level + 1)
     return img2
 
+def getAllFilters(img):
+    filters = []
+    for k in range(3):
+        img2 = np.zeros((len(img) / 3, len(img[0]) - 20, 1), np.uint8)
+        for i in range(len(img2)):
+            for j in range(len(img2[0])):
+                if k == 0:
+                    img2[i][j] = img[i + (2 * len(img2))][j]
+                if k == 1:
+                    img2[i][j] = img[i + len(img2)][j]
+                if k == 2:
+                    img2[i][j] = img[i][j]
+        filters.append(img2)
+    return filters
+
+def getAllPyramids(img , level):
+    G = img.copy()
+    gpA = [G]
+    for i in xrange(level):
+        G = cv2.pyrDown(G)
+        gpA.append(G)
+    return gpA
+
+def reconstruct(colorPyramid,level):
+    im = colorPyramid[level]
+    for i in range(len(colorPyramid) - 2, -1, -1):
+        im = cv2.pyrUp(im)
+        im = cv2.resize(im, (colorPyramid[i].shape[1], colorPyramid[i].shape[0]))
+        im = cv2.add(im, colorPyramid[i])
+    return im
+
+level = 4
 if len(sys.argv) < 2:
     print("Not Enough Argument")
     sys.exit(0)
 
 img = cv2.imread(sys.argv[1],0)
-while True:
-    img2 = reconstruct(img)
-    img2 = removeBorder(img2)
-    cv2.imshow('Processed Image' ,img2)
-    k = cv2.waitKey(0)
-    if k == ord('q') or k == ord('Q'):
-        break
-cv2.destroyAllWindows()
+filters = getAllFilters(img)
+pyramids = []
+for filter in filters:
+    pyramids.append(getAllPyramids(filter,level))
+colorPyramid = []
+for i in range(len(pyramids[0])):
+    colorPyramid.append(getColor(pyramids[2][i],pyramids[1][i],pyramids[0][i],level))
+
+im = reconstruct(colorPyramid,level)
+
+im = removeBorder(im)
+
+cv2.imshow('Image',im)
+k = cv2.waitKey(0)
+if k == ord('q') or k == ord('Q'):
+    cv2.destroyAllWindows()
